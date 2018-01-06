7abf77699ad897bd0a747dd8f9758527bb65301f
Zohaib Ahmed <zohaibahmedbutt@gmail.com>
2018-01-06 19:29:45 +0500

    Implemented Burger and Burger Ingredient components

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import AuxiliaryHOC from '../../hoc/Aux';
+import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
   render() {
     return (
       <AuxiliaryHOC>
-        <div>Burger</div>
+        <Burger/>
         <div>Build Controls</div>
       </AuxiliaryHOC>
     );
