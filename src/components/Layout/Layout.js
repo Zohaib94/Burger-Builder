be063d6f24b78cf1b5925da1c4dace1e22a299c7
Zohaib Ahmed <zohaibahmedbutt@gmail.com>
2018-01-06 18:58:37 +0500

    Added CSS for Layout Component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import AuxiliaryHOC from '../../hoc/Aux';
+import classes from './Layout.css';
 
 const Layout = (props) => (
   <AuxiliaryHOC>
     <div>Navigational Components will be added here (Toolbar, Sidebar etc.)</div>
-    <main>{props.children}</main>
+    <main className='main'>{props.children}</main>
   </AuxiliaryHOC>
 );
 
