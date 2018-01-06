98a8f8c3ffd1e31570f0900754582ebd5ab468e2
Zohaib Ahmed <zohaibahmedbutt@gmail.com>
2018-01-07 00:50:09 +0500

    Display Burger Contents based on state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -2,13 +2,24 @@ import React from 'react';
 import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-const burger = (props) => (
+const burger = (props) => {
+  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    var ingredientCount = props.ingredients[igKey];
+    var ingredientsList = [];
+
+    for(var i = 0; i < ingredientCount; i++){
+      ingredientsList.push(<BurgerIngredient key={igKey + i} type={igKey} />);
+    }
+
+    return ingredientsList;
+  });
+  return (
     <div className='Burger'>
       <BurgerIngredient type="bread-top"/>
-      <BurgerIngredient type="cheese"/>
-      <BurgerIngredient type="meat"/>
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom"/>
     </div>
-)
+  );
+}
 
 export default burger;
