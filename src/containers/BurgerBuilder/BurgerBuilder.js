36601a23adef640f13f5158fe9caf00fbcf3d16a
Zohaib Ahmed <zohaibahmedbutt@gmail.com>
2018-01-07 14:22:40 +0500

    Added BuildControls and BuildControl Components

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import AuxiliaryHOC from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 
 class BurgerBuilder extends Component {
   state = {
@@ -15,6 +16,7 @@ class BurgerBuilder extends Component {
     return (
       <AuxiliaryHOC>
         <Burger ingredients={this.state.ingredients} />
+        <BuildControls />
       </AuxiliaryHOC>
     );
   }
