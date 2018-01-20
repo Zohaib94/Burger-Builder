580d729e94b920188d39680059161a1cd4cfc76b
Zohaib Ahmed <zohaib.ahmed@square63.com>
2018-01-20 15:48:49 +0500

    Updates

--- src/components/UI/Backdrop/Backdrop.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './Backdrop.css'
-
-const backdrop = (props) => (
-  props.show_backdrop ? <div className='Backdrop' onClick={() => props.toggleDisplay()}></div> : null
-)
-
-export default backdrop;
--- src/components/UI/CustomButton/CustomButton.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './CustomButton.css';
-
-const custombutton = (props) => (
-  <button className={['Button', props.buttonStyle].join(' ')} onClick={() => props.clickEvent()}>{props.children}</button>
-);
-
-export default custombutton;
