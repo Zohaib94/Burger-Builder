b25ac7b6f348cef873eec02622bc1c6ceb6d3dbf
Zohaib Ahmed <zohaibahmedbutt@gmail.com>
2018-01-20 14:36:26 +0500

    Updated Toolbar Styling

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -6,8 +6,7 @@ import NavigationItems from '../NavigationItems/NavigationItems';
 const toolbar = (props) => (
   <header className='Toolbar'>
     <div>Menu</div>
-    <Logo/>
-    <nav>
+    <nav className="DesktopOnly">
       <NavigationItems/>
     </nav>
   </header>
